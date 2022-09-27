5aa8ed76705fa861368643b14649497d262cc1cd
dallin <dallinwright01@gmail.com>
2022-09-27 16:22:44 +0200

    debugging

--- iam.tf
+++ iam.tf
@@ -1,56 +1,56 @@
-module "organization-iam" {
-  source  = "terraform-google-modules/iam/google//modules/organizations_iam"
-  version = "~> 7.4"
-
-  organizations = [var.org_id]
-
-  bindings = {
-
-    "roles/billing.admin" = [
-      "group:gcp-billing-admins@dallinw.com",
-    ]
-
-    "roles/resourcemanager.organizationAdmin" = [
-      "group:gcp-organization-admins@dallinw.com",
-    ]
-
-  }
-}
-
-
-module "development-iam" {
-  source  = "terraform-google-modules/iam/google//modules/folders_iam"
-  version = "~> 7.4"
-
-  folders = [google_folder.development.name]
-
-  bindings = {
-    "roles/compute.instanceAdmin.v1" = [
-      "group:gcp-developers@dallinw.com",
-    ]
-
-    "roles/container.admin" = [
-      "group:gcp-developers@dallinw.com",
-    ]
-
-  }
-}
-
-
-module "staging-iam" {
-  source  = "terraform-google-modules/iam/google//modules/folders_iam"
-  version = "~> 7.4"
-
-  folders = [google_folder.staging.name]
-
-  bindings = {
-
-    "roles/compute.instanceAdmin.v1" = [
-      "group:gcp-developers@dallinw.com",
-    ]
-
-    "roles/container.admin" = [
-      "group:gcp-developers@dallinw.com",
-    ]
-  }
-}
+#module "organization-iam" {
+#  source  = "terraform-google-modules/iam/google//modules/organizations_iam"
+#  version = "~> 7.4"
+#
+#  organizations = [var.org_id]
+#
+#  bindings = {
+#
+#    "roles/billing.admin" = [
+#      "group:gcp-billing-admins@dallinw.com",
+#    ]
+#
+#    "roles/resourcemanager.organizationAdmin" = [
+#      "group:gcp-organization-admins@dallinw.com",
+#    ]
+#
+#  }
+#}
+#
+#
+#module "development-iam" {
+#  source  = "terraform-google-modules/iam/google//modules/folders_iam"
+#  version = "~> 7.4"
+#
+#  folders = [google_folder.development.name]
+#
+#  bindings = {
+#    "roles/compute.instanceAdmin.v1" = [
+#      "group:gcp-developers@dallinw.com",
+#    ]
+#
+#    "roles/container.admin" = [
+#      "group:gcp-developers@dallinw.com",
+#    ]
+#
+#  }
+#}
+#
+#
+#module "staging-iam" {
+#  source  = "terraform-google-modules/iam/google//modules/folders_iam"
+#  version = "~> 7.4"
+#
+#  folders = [google_folder.staging.name]
+#
+#  bindings = {
+#
+#    "roles/compute.instanceAdmin.v1" = [
+#      "group:gcp-developers@dallinw.com",
+#    ]
+#
+#    "roles/container.admin" = [
+#      "group:gcp-developers@dallinw.com",
+#    ]
+#  }
+#}
--- providers.tf
+++ providers.tf
@@ -1,4 +1,20 @@
+terraform {
+  required_version = ">=0.13"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/exported-krm/v0.1.0"
+  }
+}
+
 provider "google" {
-  project     = "dallinw"
-  region      = "us-east1"
+  project = "dallinw-root"
+  region  = "us-east1"
+  zone    = "us-east1-a"
 }
