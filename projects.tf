f2cd88732904169bf1851f4d7d69b6d9b52360cc
dallin <dallinwright01@gmail.com>
2022-09-27 16:35:49 +0200

    debugging

--- projects.tf
+++ projects.tf
@@ -1,83 +1,83 @@
-#module "logging-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "logging"
-#  project_id = "logging-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "monitoring-dev-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "monitoring-dev"
-#  project_id = "monitoring-dev-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "monitoring-nonprod-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "monitoring-nonprod"
-#  project_id = "monitoring-nonprod-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "monitoring-prod-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "monitoring-prod"
-#  project_id = "monitoring-prod-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "vpc-host-dev-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "vpc-host-dev"
-#  project_id = "vpc-host-dev-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "vpc-host-nonprod-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "vpc-host-nonprod"
-#  project_id = "vpc-host-nonprod-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
-#
-#module "vpc-host-prod-kz792-gb530" {
-#  source  = "terraform-google-modules/project-factory/google"
-#  version = "~> 12.0"
-#
-#  name       = "vpc-host-prod"
-#  project_id = "vpc-host-prod-kz792-gb530"
-#  org_id     = var.org_id
-#  folder_id  = google_folder.common.name
-#
-#  billing_account = var.billing_account
-#}
+module "logging-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "logging"
+  project_id = "logging-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-dev-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-dev"
+  project_id = "monitoring-dev-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-nonprod-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-nonprod"
+  project_id = "monitoring-nonprod-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "monitoring-prod-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "monitoring-prod"
+  project_id = "monitoring-prod-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "vpc-host-dev-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-dev"
+  project_id = "vpc-host-dev-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "vpc-host-nonprod-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-nonprod"
+  project_id = "vpc-host-nonprod-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
+
+module "vpc-host-prod-kz792-gb530" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 12.0"
+
+  name       = "vpc-host-prod"
+  project_id = "vpc-host-prod-kz792-gb530"
+  org_id     = var.org_id
+  folder_id  = google_folder.common.name
+
+  billing_account = var.billing_account
+}
