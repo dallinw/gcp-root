d04908383d8e59d41a6bdea3663beeced1faed8d
Dallin Wright <dallin.wright@supportlogic.io>
2022-10-03 17:12:32 +0200

    debugging keys

--- projects.tf
+++ projects.tf
@@ -29,7 +29,7 @@ module "development" {
   name       = "development"
   project_id = "development-${var.project_suffix}"
   org_id     = var.org_id
-  folder_id  = google_folder.common.name
+  folder_id  = google_folder.development.name
 
   billing_account = var.billing_account
 }
